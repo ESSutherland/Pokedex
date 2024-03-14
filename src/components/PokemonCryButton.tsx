13151e1745fe5284825b10e914424babe82d0371
Ethan Sutherland <ESutherland95@gmail.com>
2024-03-14 18:58:21 -0500

    added a cry button to play the pokemon cry

---
+++ src/components/PokemonCryButton.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { HiSpeakerWave } from "react-icons/hi2";
+import { usePokemonContext } from "../context/PokemonContext";
+
+const PokemonCryButton = () => {
+  const { pokemonData } = usePokemonContext();
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [audio] = useState(
+    new Audio(
+      `https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/${pokemonData?.id}.ogg`
+    )
+  );
+
+  const handleAudioClick = () => {
+    audio.volume = 0.15;
+    audio.play();
+    setIsPlaying(true);
+    audio.onended = () => {
+      setIsPlaying(false);
+    };
+  };
+
+  return (
+    <div
+      className={`p-3 rounded-full mb-6 border-4 border-black/20 flex items-center justify-center hover:cursor-pointer ${
+        isPlaying ? "bg-green-400" : "bg-slate-200"
+      }`}
+      onClick={() => handleAudioClick()}
+    >
+      <HiSpeakerWave className="w-[25px] h-[25px] opacity-70" />
+    </div>
+  );
+};
+
+export default PokemonCryButton;

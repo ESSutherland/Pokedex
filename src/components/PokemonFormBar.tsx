925dfee4562e00ea56f83cdac86204d8d1f015a5
Ethan Sutherland <ESutherland95@gmail.com>
2024-03-14 23:02:07 -0500

    extracted for bar into sperate component

---
+++ src/components/PokemonFormBar.tsx
@@ -0,0 +1,107 @@
+import { usePokemonContext } from "../context/PokemonContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
+
+const PokemonFormBar = () => {
+  const {
+    varietyIndex,
+    formIndex,
+    getEnglishName,
+    currentForm,
+    varietiesList,
+    formsList,
+    speciesData,
+    updateVariety,
+    updateForm,
+  } = usePokemonContext();
+
+  const getFormName = () => {
+    if (varietiesList && formsList) {
+      let name =
+        getEnglishName(currentForm?.form_names).length > 0
+          ? getEnglishName(currentForm?.form_names)
+          : getEnglishName(speciesData?.names);
+      let formName =
+        varietiesList.length > 1 || (formsList && formsList.length > 1) ? (
+          varietiesList.length > formsList.length ? (
+            <div className="formsList">
+              {name} (Form {varietyIndex + 1} of {varietiesList.length})
+            </div>
+          ) : (
+            <div className="formsList">
+              {name} (Form {formIndex + 1} of {formsList.length})
+            </div>
+          )
+        ) : (
+          <div className="forms">No Alternate Forms</div>
+        );
+
+      return formName;
+    }
+    return;
+  };
+
+  const handlePrevVarietyClick = () => {
+    if (varietiesList && formsList && varietiesList.length > formsList.length) {
+      if (varietyIndex > 0) {
+        updateVariety(varietyIndex - 1);
+      }
+    } else {
+      if (formIndex > 0) {
+        updateForm(formIndex - 1);
+      }
+    }
+  };
+
+  const handleNextVarietyClick = () => {
+    if (varietiesList && formsList && varietiesList.length > formsList.length) {
+      if (varietyIndex + 1 < varietiesList.length) {
+        updateVariety(varietyIndex + 1);
+      }
+    } else {
+      if (formsList && formIndex + 1 < formsList.length) {
+        updateForm(formIndex + 1);
+      }
+    }
+  };
+
+  const disablePrevButton = () => {
+    if (varietiesList && formsList && varietiesList.length > formsList.length) {
+      return varietyIndex === 0;
+    } else {
+      return formIndex === 0;
+    }
+  };
+
+  const disableNextButton = () => {
+    if (varietiesList && formsList && varietiesList.length > formsList.length) {
+      return varietyIndex === varietiesList.length - 1;
+    } else {
+      return formsList && formIndex === formsList.length - 1;
+    }
+  };
+
+  return (
+    <div className="flex justify-between items-center w-full max-w-[400px] text-slate-200 font-semibold text-xl bg-black/50 sm:rounded-lg">
+      <button
+        type="button"
+        onClick={handlePrevVarietyClick}
+        disabled={disablePrevButton()}
+        className="disabled:text-slate-400 flex items-center justify-center"
+      >
+        <FontAwesomeIcon icon={faCaretLeft} className={"w-[30px] h-[30px]"} />
+      </button>
+      <div className="w-[350px] text-center text-sm">{getFormName()}</div>
+      <button
+        type="button"
+        onClick={handleNextVarietyClick}
+        disabled={disableNextButton()}
+        className="disabled:text-slate-400 flex items-center justify-center"
+      >
+        <FontAwesomeIcon icon={faCaretRight} className="w-[30px] h-[30px]" />
+      </button>
+    </div>
+  );
+};
+
+export default PokemonFormBar;
--- src/components/PokemonMainContent.tsx
+++ src/components/PokemonMainContent.tsx
@@ -2,63 +2,31 @@ import { useEffect, useState } from "react";
 import PokemonImage from "./PokemonImage";
 import PokemonHeader from "./PokemonHeader";
 import PokemonTypes from "./PokemonTypes";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
 import { usePokemonContext } from "../context/PokemonContext";
 import PokemonInfo from "./PokemonInfo";
 import PokemonShinyButton from "./PokemonShinyButton";
 import PokemonCryButton from "./PokemonCryButton";
+import PokemonFormBar from "./PokemonFormBar";
 
 const PokemonMainContent = () => {
   const [isShiny, setIsShiny] = useState(false);
-  const {
-    isLoading,
-    varietyIndex,
-    updateVariety,
-    pokemonId,
-    getEnglishName,
-    pokemonData,
-    speciesData,
-    varietiesList,
-    formsList,
-  } = usePokemonContext();
+  const { isLoading, varietyIndex, pokemonId, currentForm, formIndex } =
+    usePokemonContext();
 
   useEffect(() => {
     setIsShiny(false);
-  }, [pokemonId, varietyIndex]);
+  }, [pokemonId, varietyIndex, formIndex]);
 
   const handleShinyClick = () => {
     setIsShiny((c) => !c);
   };
 
-  const handlePrevVarietyClick = () => {
-    if (varietyIndex !== 0) updateVariety(varietyIndex - 1);
-  };
-
-  const handleNextVarietyClick = () => {
-    if (varietiesList && varietyIndex < varietiesList?.length)
-      updateVariety(varietyIndex + 1);
-  };
-
-  const getFormName = () => {
-    const formName =
-      varietiesList && formsList && varietiesList?.length > 1
-        ? formsList[0].form_names.length > 0
-          ? getEnglishName(formsList[0].names) +
-            ` (Form ${varietyIndex + 1} of ${varietiesList?.length})`
-          : getEnglishName(speciesData?.names) +
-            ` (Form ${varietyIndex + 1} of ${varietiesList?.length})`
-        : "No Forms";
-
-    return formName;
-  };
-
   const getFirstType = () => {
-    return pokemonData?.types[0].type.name || "normal";
+    return currentForm?.types[0].type.name || "normal";
   };
 
   return (
-    <div className=" flex justify-center w-full h-[600px] overflow-x-clip relative sm:mt-4">
+    <div className=" flex justify-center items-center w-full overflow-x-clip relative flex-col xl:flex-row sm:mt-4 gap-5">
       <div
         className="flex absolute w-[100rem] h-[100rem] rounded-full -top-[55rem] left-1/2 -translate-x-[28rem] -z-[1] items-center border-2 border-black/10 transition-all"
         style={{
@@ -68,9 +36,9 @@ const PokemonMainContent = () => {
         <div className="w-[90%] h-[90%] bg-black/40 rounded-full ml-3 "></div>
       </div>
       {isLoading ? (
-        <div className="h-[300px] w-[300px] bg-[url('./assets/pokeball.png')] bg-center bg-no-repeat bg-contain animate-spin mt-32"></div>
+        <div className="h-[300px] w-[300px] bg-[url('./assets/pokeball.png')] bg-center bg-no-repeat bg-contain animate-spin mt-32 xl:ml-[500px]"></div>
       ) : (
-        <div className="flex flex-col w-full max-w-[600px] h-[600px] items-center relative">
+        <div className="flex flex-col w-full max-w-[600px] h-[600px] items-center relative xl:ml-[500px]">
           <PokemonHeader />
           <PokemonTypes />
           <div className="flex flex-1 justify-center">
@@ -83,33 +51,7 @@ const PokemonMainContent = () => {
             />
             <PokemonCryButton />
           </div>
-          <div className="flex justify-between items-center w-full text-slate-200 font-semibold text-xl bg-black/50 sm:rounded-lg">
-            <button
-              type="button"
-              onClick={handlePrevVarietyClick}
-              disabled={varietyIndex === 0}
-              className="disabled:text-slate-400 flex items-center justify-center"
-            >
-              <FontAwesomeIcon
-                icon={faCaretLeft}
-                className={"w-[30px] h-[30px]"}
-              />
-            </button>
-            <div className="w-[350px] text-center text-lg">{getFormName()}</div>
-            <button
-              type="button"
-              onClick={handleNextVarietyClick}
-              disabled={
-                varietiesList && varietyIndex === varietiesList?.length - 1
-              }
-              className="disabled:text-slate-400 flex items-center justify-center"
-            >
-              <FontAwesomeIcon
-                icon={faCaretRight}
-                className="w-[30px] h-[30px]"
-              />
-            </button>
-          </div>
+          <PokemonFormBar />
         </div>
       )}
       <PokemonInfo />

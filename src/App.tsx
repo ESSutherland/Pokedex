e46f0f87dd83e8ccf12db32bba4876eeeafd1e55
Ethan Sutherland <ESutherland95@gmail.com>
2024-03-15 23:20:59 -0500

    updated styling

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ function App() {
   const [listActive, setListActive] = useState(false);
   return (
     <>
-      <div className="flex mt-16 lg:mt-0 justify-center w-full relative min-h-[1200px] overflow-hidden">
-        <div className="w-full lg:mx-36 relative flex flex-col items-center">
+      <div className="flex mt-16 xl:mt-0 justify-center w-full relative min-h-[1200px] overflow-hidden">
+        <div className="w-full xl:mx-36 relative flex flex-col items-center">
           <PokemonContextProvider>
             <PokemonMainContent />
             <PokemonScrollBar active={listActive} />
@@ -22,11 +22,11 @@ function App() {
           </PokemonContextProvider>
           <div className="absolute top-0 -z-10 w-full h-[5000px] bg-slate-300 dark:bg-slate-800"></div>
         </div>
-        <div className="bg-slate-400 dark:bg-slate-900 w-full h-16 fixed top-0 lg:bg-slate-400 lg:w-[64px] lg:rounded-full lg:left-40 lg:top-4 shadow-xl">
+        <div className="bg-slate-400 dark:bg-slate-900 w-full h-16 fixed top-0 xl:bg-slate-400 xl:w-[64px] xl:rounded-full xl:left-40 xl:top-4 shadow-xl">
           <button
             type="button"
             onClick={() => setListActive(!listActive)}
-            className="absolute top-2 right-4 lg:left-2 z-10 text-white h-12 w-12 flex items-center justify-center"
+            className="absolute top-2 right-4 xl:left-2 z-10 text-white h-12 w-12 flex items-center justify-center"
           >
             {listActive ? (
               <IoClose className="w-[90%] h-[90%]" />
